6b53ae076b0eb06d3b184677e495d207c0d05968
chopin0807 <chopin0807@naver.com>
2023-09-15 17:29:10 +0900

    원할한 크롤링을 위한 URL접속 경로와 요청 헤더 설정 후 크롤링 테스트

--- samsung_stock_crawling.py
+++ samsung_stock_crawling.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get("https://finance.naver.com/item/sise.nhn?code=005930")
-soup = BeautifulSoup(response.text, 'html.parser')
+# response = requests.get("https://finance.naver.com/item/sise.nhn?code=005930")
+response = requests.get("https://finance.naver.com/item/sise_day.naver?code=005930&page=1", headers={"User-agent": "Mozilla/5.0"})
+soup = BeautifulSoup(response.text,'html.parser')
 print(soup)
