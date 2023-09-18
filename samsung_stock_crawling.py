1ac9a70acf983475cf1fa4893d7688120b76c05f
unknown <chopin0807@naver.com>
2023-09-18 10:01:33 +0900

    수치데이터 규칙 확인 후 정렬 예정

--- samsung_stock_crawling.py
+++ samsung_stock_crawling.py
@@ -22,10 +22,12 @@ for i in date_find:
 
 print("날짜: ", date)
 
-# 종가
-endprice_find = soup.select("tr > .num > span.p11")
+# 수치데이터
+num_data_find = soup.select("tr > .num > span.p11")
 endprice = []
-for i in endprice_find:
+for i in num_data_find:
     endprice.append(i.text)
 
-print("종가: ", endprice)
+print("수치데이터: ", endprice)
+
+# 수치데이터 => index번호 // 6 == (0): 종가, (1): 전일비, (2): 시가, (3): 고가, (4): 저가, (5): 거래량
