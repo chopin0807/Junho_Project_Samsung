b6b08ba17176a9577b185c38d62e1e2bc758492c
unknown <chopin0807@naver.com>
2023-09-19 12:21:23 +0900

    중간저장

--- naver_news_crawling.py
+++ naver_news_crawling.py
@@ -5,9 +5,17 @@
 
 import requests
 from bs4 import BeautifulSoup
+import re # 정규표현식 사용
 
 response = requests.get("https://news.naver.com/main/list.naver?mode=LPOD&mid=sec&oid=001&date=20230831&page=1", headers={"User-agent": "Mozilla/5.0"})
 soup = BeautifulSoup(response.text, "html.parser")
+# 현재 페이지 확인
+page_find = soup.select_one(".paging")
+page_text = str(page_find)
+page_result = re.search("(?<=<strong>)[0-9]*", page_text)
+page_result = page_result.group()
+page_result = int(page_result)
+print("현재 페이지: ", page_result)
 
 # 기사제목
 title_find = soup.select("dl > dt > a")
@@ -33,4 +41,32 @@ for i in date:
 
 # 기사본문, 반응 확인하기 위한 url크롤링
 url_find = soup.select("dt > a")
-print(url_find)
+url = []
+for i in url_find:
+    url.append(i.attrs["href"])
+
+# url 중복 제거
+url_result = []
+for i in url:
+    if i not in url_result:
+        url_result.append(i)
+
+# print("기사 url: ", url_result)
+
+# 기사본문/기사반응
+# 검색된 기사 url에 대한 각각의 크롤링 후 기사본문/기사반응 수집
+for article in url_result:
+    article_response = requests.get(article, {"User-agent": "Mozilla/5.0"})
+    article_soup = BeautifulSoup(article_response.text, "html.parser")
+
+    # 기사본문
+    text_find = article_soup.select("div > article") # 스포츠외의 기사
+    sport_text = article_soup.select(".content_area > .news_end")
+    
+    for text in sport_text:
+        # print(text.text.strip())
+        text_result = re.sub("[^0-9가-힣].[a-z0-9]+@[a-z0-9.]+.[(\[].*[)\]]", "", text.text.strip())
+        text_result = re.sub("[^0-9가-힣].[a-z0-9]+@[a-z0-9.]*", "", text_result)
+        text_find = re.findall(".*\n(?=기사제공 연합뉴스)", text_result)
+        print(text_find)
+        print("============================================================================================================")
